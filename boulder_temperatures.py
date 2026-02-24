b789d07255240d2599f6e1a998ef0c92b96a6659
Ignacio Naus <igna9555@colorado.edu>
2026-02-24 10:41:28 -0700

    Add my name

--- boulder_temperatures.py
+++ boulder_temperatures.py
@@ -10,7 +10,7 @@ Instructions:
 """
 
 # TODO: Add your name here
-STUDENT_NAME = ""  # Example: "Alice Johnson"
+STUDENT_NAME = "Ignacio Naus"  # Example: "Alice Johnson"
 
 
 def load_temperatures(filepath):
